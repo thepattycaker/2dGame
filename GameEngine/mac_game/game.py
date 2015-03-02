bd0c46f3ea344aa019055e5b2ff4476d5dbe94ca
Radahika <radhika.marvin@berkeley.edu>
2015-03-01 21:05:53 -0800

    cleaned up game

--- GameEngine/mac_game/game.py
+++ GameEngine/mac_game/game.py
@@ -24,11 +24,11 @@ def center_image(image):
     image.anchor_x = image.width/2
     image.anchor_y = image.height/2
 
-def distance(point_1=(0, 0), point_2=(0, 0)): 
+def distance(point_1=(0, 0), point_2=(0, 0)):
     return math.sqrt( (point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
 
-def collides_with(player_object, other_object): 
-    collision_distance = player_object.image.width/2 + other_object.image.width/2 
+def collides_with(player_object, other_object):
+    collision_distance = player_object.image.width/2 + other_object.image.width/2
     actual_distance = distance(player_object.position, other_object.position) 
     return (actual_distance <= collision_distance)
 
